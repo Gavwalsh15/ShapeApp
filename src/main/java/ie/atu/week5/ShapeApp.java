b13a4bdd961866ca1db23c46e08104564ecf28ff
G00402296 <G00402296@atu.ie>
2023-01-07 15:29:48 +0000

    Project Created

--- src/main/java/ie/atu/week5/ShapeApp.java
+++ src/main/java/ie/atu/week5/ShapeApp.java
@@ -1,4 +1,16 @@
 package ie.atu.week5;
 
+import java.util.Scanner;
+
 public class ShapeApp {
+    public static void main(String[] args) {
+        System.out.println("Please Enter the lenght of the rectangle");
+        Scanner input = new Scanner(System.in);
+        double length = input.nextDouble();
+        System.out.println("You entered :"+ length);
+
+        System.out.println("Please Enter the width of the rectangle");
+        double width = input.nextDouble();
+        System.out.println("You entered :"+ width);
+    }
 }
